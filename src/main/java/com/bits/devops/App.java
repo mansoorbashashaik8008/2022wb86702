be5ca59582c36c464d6b1bfd3c3a4f881966d5fa
Mansoor_Basha_Shaik <mansoorbashashaik8008@gmail.com>
2026-09-25 06:21:39 +0530

    Update application for Jenkins CI demonstration

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("BITS DevOps Lab - Continuous Integration");
         System.out.println("BITS ID: 2022WB86702");
+        System.out.println("Jenkins CI - SCM Change Detected");
     }
 }
