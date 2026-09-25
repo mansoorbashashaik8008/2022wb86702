6d9f1d8457c5f25577720437c0d800e9c7013b46
Mansoor_Basha_Shaik <mansoorbashashaik8008@gmail.com>
2026-09-25 06:10:32 +0530

    Initial DevOps lab project

---
+++ README.md
@@ -0,0 +1,6 @@
+# BITS DevOps Lab - 2022WB86702
+
+This project demonstrates:
+- Maven continuous build
+- Git version control
+- Jenkins continuous integration
---
+++ src/main/java/com/bits/devops/App.java
@@ -0,0 +1,8 @@
+package com.bits.devops;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("BITS DevOps Lab - Continuous Integration");
+        System.out.println("BITS ID: 2022WB86702");
+    }
+}
